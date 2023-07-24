4543d9d05d695622e7b6491b0e292b955db0ba27
Harald Vohringer <harald.voeh@gmail.com>
2023-07-24 06:40:45 +0200

    implemented handleIncrement and getBadgeClasses

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,6 +4,7 @@ class Counter extends Component {
   state = {
     count: 2,
     // imageUrl: "http://picsum.photos/200",
+    tags: [],
   };
 
   //   styles = {
@@ -12,16 +13,37 @@ class Counter extends Component {
   //   };
 
   render() {
-    let classes = this.getBadgeClasses();
-
     return (
       <div>
         {/* <img src={this.state.imageUrl} alt=""></img> */}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {this.renderTags()}
       </div>
     );
   }
+
+  renderTags() {
+    if (this.state.tags.length == 0) return <p> There are no tags </p>;
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  handleIncrement = () => {
+    console.log("Increment Clicked", this);
+    this.setState({ count: this.state.count + 1 });
+  };
+
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
     classes += this.state.count === 0 ? "warning" : "primary";
