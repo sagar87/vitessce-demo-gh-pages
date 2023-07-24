e09adc8c9bfcbec1857db0bfea012b6f9530a385
Harald Vohringer <harald.voeh@gmail.com>
2023-07-24 06:43:11 +0200

    Added and imported font-awesome

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
 import Counter from "./components/counter";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
