c0b3d033f18a0b32d46021c5006eb2e7cce73d24
Harald Vohringer <harald.voeh@gmail.com>
2023-07-27 11:09:43 +0200

    added a new sample

--- src/components/configs.jsx
+++ src/components/configs.jsx
@@ -158,4 +158,57 @@ export const myViewConfig = {
       },
     ],
   },
+  "166_1_E2_FL3a.ome.tiff": {
+    version: "1.0.6",
+    name: "HBM836.VTFP.364",
+    description:
+      "Periodic acid-Schiff stained microscopy collected from the right kidney.",
+    public: true,
+    datasets: [
+      {
+        uid: "HBM836.VTFP.3641",
+        name: "HBM836.VTFP.3641",
+        files: [
+          {
+            fileType: "image.ome-tiff",
+            url: "http://localhost:9000/166_1_E2_FL3a.ome.tiff",
+          },
+        ],
+      },
+    ],
+    initStrategy: "auto",
+    layout: [
+      {
+        component: "spatial",
+        x: 0,
+        y: 0,
+        w: 9,
+        h: 12,
+      },
+      {
+        component: "layerController",
+        props: {
+          disableChannelsIfRgbDetected: true,
+        },
+        x: 9,
+        y: 0,
+        w: 3,
+        h: 6,
+      },
+      {
+        component: "description",
+        x: 9,
+        y: 6,
+        w: 3,
+        h: 3,
+      },
+      {
+        component: "status",
+        x: 9,
+        y: 9,
+        w: 3,
+        h: 3,
+      },
+    ],
+  },
 };
